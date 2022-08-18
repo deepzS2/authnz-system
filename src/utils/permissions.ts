a43c7c0ce226586ba0d0cda573d3f98fe583e587
deepzS2 <alanbackman10@hotmail.com>
2022-08-18 16:24:37 -0300

    📝: Adding documentation

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -6,29 +6,57 @@ export enum Permissions {
   Owner = 1 << 4,
 }
 
-type PermissionsJSON = {
-  -readonly [K in keyof typeof Permissions]: boolean;
-};
+type PermissionsJSON = Omit<
+  {
+    -readonly [K in keyof typeof Permissions]: boolean;
+  },
+  number
+>;
 
 export class PermissionUtil {
+  /**
+   * A class to use the most common permission functions!
+   * @param permission Combinations of the permissions enums
+   * @example
+   * import { PermissionUtil, Permissions } from '@utils/permissions'
+   *
+   * const permission = new PermissionUtil(Permissions.None + Permissions.Read)
+   */
   constructor(public permission: number) {}
 
+  /**
+   * Check the permissions of the permission prop
+   * @param permissionToCheck Permission enum to check
+   * @returns True if has permission
+   */
   hasPermission(permissionToCheck: Permissions) {
     return !!(permissionToCheck & this.permission);
   }
 
+  /**
+   * Add permission to the permission prop
+   * @param permissionToAdd Permission enum to add
+   */
   addPermission(permissionToAdd: Permissions) {
     this.permission |= permissionToAdd;
 
     return this;
   }
 
+  /**
+   * Remove permission to the permission prop
+   * @param permissionToRemove Permission enum to remove
+   */
   removePermission(permissionToRemove: Permissions) {
     this.permission ^= permissionToRemove;
 
     return this;
   }
 
+  /**
+   * Transforms the enum to a object checking for each permission key
+   * @returns Object with permission keys and if has permission
+   */
   toJSON(): PermissionsJSON {
     const keys = Object.keys(Permissions) as Array<keyof typeof Permissions>;
 
